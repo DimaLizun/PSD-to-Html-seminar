ee65e37251d815731b1a19acfe64ebd5766c27de
Dima Lizun <dimalizun@gmail.com>
2017-03-15 03:22:30 +0300

    add sections

--- app/js/common.js
+++ app/js/common.js
@@ -7,49 +7,6 @@ $(function() {
 		});
 	};
 
-	// mobile menu
-	var $menu = $("#my-menu").mmenu({
-   //   options
- 	});
-	var $icon = $("#my-icon");
-	var API = $menu.data( "mmenu" );
-
-	$icon.on( "click", function() {
-		API.open();
-	});
-
-	API.bind( "opened", function() {
-		setTimeout(function() {
-			$icon.addClass( "is-active" );
-		}, 100);
-	});
-	API.bind( "closed", function() {
-		setTimeout(function() {
-			$icon.removeClass( "is-active" );
-		}, 100);
-	});
-
-
-	// fuul view block
-	function showElem(valueAttr) {
-		var name = $(valueAttr).attr('name');
-		var display = $(name).css('display');
-
-		if (display == 'none')
-			$(name).css("display", "inline-block");
-		 else 
-			$(name).css("display", "none");
-	};
-
-	$('.show-work').click(function(){
-		showElem('.show-work');
-	});
-
-	$('.show-blog').click(function(){
-		showElem('.show-blog');
-	});
-
-
   // scroll
 	$('a[href^="#"]').click(function () {
 		event.preventDefault();
